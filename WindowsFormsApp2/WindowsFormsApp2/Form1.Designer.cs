8f27b7f22176a7c0bce7b1e58306d2f06359921a
Muhammed Eryılmaz <147308872+MuhEry@users.noreply.github.com>
2024-11-20 20:53:39 +0300

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.toplamMayınLabel = new System.Windows.Forms.Label();
+            this.kalanHamleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(90, 75);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(665, 615);
+            this.panel1.TabIndex = 0;
+            // 
+            // toplamMayınLabel
+            // 
+            this.toplamMayınLabel.AutoSize = true;
+            this.toplamMayınLabel.Font = new System.Drawing.Font("Rockwell", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toplamMayınLabel.Location = new System.Drawing.Point(85, 20);
+            this.toplamMayınLabel.Name = "toplamMayınLabel";
+            this.toplamMayınLabel.Size = new System.Drawing.Size(219, 33);
+            this.toplamMayınLabel.TabIndex = 1;
+            this.toplamMayınLabel.Text = "Toplam Mayın";
+            // 
+            // kalanHamleLabel
+            // 
+            this.kalanHamleLabel.AutoSize = true;
+            this.kalanHamleLabel.Font = new System.Drawing.Font("Rockwell", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kalanHamleLabel.Location = new System.Drawing.Point(498, 20);
+            this.kalanHamleLabel.Name = "kalanHamleLabel";
+            this.kalanHamleLabel.Size = new System.Drawing.Size(198, 33);
+            this.kalanHamleLabel.TabIndex = 2;
+            this.kalanHamleLabel.Text = "Kalan Hücre:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(842, 753);
+            this.Controls.Add(this.kalanHamleLabel);
+            this.Controls.Add(this.toplamMayınLabel);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label toplamMayınLabel;
+        private System.Windows.Forms.Label kalanHamleLabel;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        HashSet<Point> mayınlar = new HashSet<Point>();
+        HashSet<Point> bosAlanlar = new HashSet<Point>();
+        Random rastgele = new Random();
+        int mayınYok= 0;
+        int toplamMayın = 80;
+        int yerleştirilenMayın = 0;
+        int mayınKoy = 0;
+        int ilkTıklama = 0;
+        bool renk = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            butonlarıEkle();
+        }
+
+        public void butonlarıEkle()
+        {
+            for (int x = 0; x < panel1.Width; x += 25)
+            {
+                renk = !renk;
+                for (int y = 0; y < panel1.Height; y += 25)
+                {
+                    yeniButon(new Point(x, y), renk);
+                    renk = !renk;
+                }
+            }
+            toplamMayınLabel.Text = ("Toplam Mayın: " + yerleştirilenMayın.ToString());
+        }
+
+        public void yeniButon(Point konum, bool rengi)
+        {
+            Button btn = new Button();
+            btn.Name = "mayın" + konum.X + konum.Y;
+            btn.Size = new Size(25, 25);
+            btn.Location = (konum);
+            btn.FlatStyle = FlatStyle.Flat;
+            btn.FlatAppearance.BorderSize = 0;
+            if (renk) btn.BackColor = Color.FromArgb(95,170,20);
+            else btn.BackColor = Color.YellowGreen;
+            btn.Cursor = Cursors.Hand;
+
+            mayınKoy = rastgele.Next(0, 100);
+            if (yerleştirilenMayın < toplamMayın && mayınKoy < 14)
+            {
+                // Butonların rastgele olarak Tag değerini değiştirerek mayınları yerleştirmiş oluyoruz 
+                btn.Tag = true;
+                yerleştirilenMayın++;
+                mayınlar.Add(btn.Location);
+            }
+            else btn.Tag = false;
+            btn.Click += new EventHandler(btnTıkla);
+            btn.MouseDown += new MouseEventHandler(btnSağTıkla);
+            panel1.Controls.Add(btn);
+        }
+        void btnTıkla(object sender, EventArgs e)
+        {
+            mayınYok++;
+            kalanHamleLabel.Text = ("Kalan Hücre: " + (400-yerleştirilenMayın-mayınYok).ToString());
+            if (mayınYok == 400-yerleştirilenMayın)
+            {
+                MessageBox.Show("Oyunu Kazandınız", "Oyun Bitti!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                YenidenBaslat();
+                return;
+            }
+            Button btn = (sender as Button);
+            if (ilkTıklama == 0) // ilk tıklamada mayına denk gelmesi durumunda oradaki mayın silinir
+            {
+                if ((bool)btn.Tag)
+                {
+                    yerleştirilenMayın--;
+                    toplamMayınLabel.Text = ("Toplam Mayın: " + yerleştirilenMayın.ToString());
+                    btn.Tag = false;
+                }
+            }
+            if ((bool)btn.Tag)
+            {
+                MayınlarıGöster();
+                MessageBox.Show("Oyunu Kaybettiniz", "Oyun Bitti!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                YenidenBaslat();
+            }
+            else
+            {
+                btn.BackColor = Color.FromArgb(180, 180, 180);
+                int etrafMayınSayısı = etrafındakiMayınlar(btn.Location);
+                if (etrafMayınSayısı != 0) btn.Text = etrafMayınSayısı.ToString();
+                btn.Enabled = false;
+            }    
+        }
+        void btnSağTıkla(object sender, MouseEventArgs e)
+        {
+            Button btn = (sender as Button);
+
+            if (e.Button == MouseButtons.Right)
+            {
+                if (btn.Text.Equals("X")) btn.Text = "";
+                else btn.Text = "X";
+            }
+            
+        }
+        void MayınlarıGöster()
+        {
+            foreach (Point konum in mayınlar)
+            {
+                Button btn = panel1.Controls.OfType<Button>().FirstOrDefault(b => b.Location == konum);
+                btn.Text = "X";
+            }
+        }
+
+        public int etrafındakiMayınlar (Point konum)
+        {
+            if (bosAlanlar.Contains(konum)) return 0;
+
+            bosAlanlar.Add(konum);
+
+            int etraftakiMayın = 0;
+
+            int[] komsuX = {-25, 0 , 25};
+            int[] komsuY = { -25, 0, 25 };
+
+            foreach (int komsununX in komsuX)
+            {
+                foreach (int komsununY in komsuY)
+                {
+                    if (komsununX == 0 && komsununY == 0) continue;
+
+                    Point komsuKonum = new Point(konum.X + komsununX, konum.Y + komsununY);
+
+                    // panel1 içerisindeki butonların konumlarını komsuKonum ile karşılaştırır
+                    // ve eşleşen durum olursa o konumdaki butonu döndürür
+                    Button komsuButon = panel1.Controls.OfType<Button>().FirstOrDefault(b => b.Location == komsuKonum);
+
+                    if (komsuButon != null && (bool)komsuButon.Tag) etraftakiMayın++;
+
+                }
+            }
+            if(etraftakiMayın == 0)
+            {
+                ilkTıklama++;
+                foreach (int komsununX in komsuX)
+                {
+                    foreach (int komsununY in komsuY)
+                    {
+                        if (komsununX == 0 && komsununY == 0) continue;
+
+                        Point komsuKonum = new Point(konum.X + komsununX, konum.Y + komsununY);
+
+                        Button komsuButon = panel1.Controls.OfType<Button>().FirstOrDefault(b => b.Location == komsuKonum);
+
+                        if (komsuButon != null && !bosAlanlar.Contains(komsuKonum)) komsuButon.PerformClick();
+
+                    }
+                }
+            }
+            return etraftakiMayın;
+        }
+        public void YenidenBaslat()
+        {
+            mayınYok = 0;
+            yerleştirilenMayın = 0;
+            ilkTıklama = 0;
+            mayınlar.Clear();
+            bosAlanlar.Clear();
+            panel1.Controls.Clear();
+            butonlarıEkle();
+            kalanHamleLabel.Text = ("Kalan Hücre: " + (400 - yerleştirilenMayın - mayınYok).ToString());
+        }
+    }
+}
